f0570bd8305636186d9f860949bdd19ef35adece
Zach Stone <zach@giantswarm.io>
2021-06-24 14:17:41 +0200

    Debug stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,16 @@ if [ -f "$GITHUB_WORKSPACE/$INPUT_CONFIG_PATH" ]; then
   CONFIG=" --config-path=$GITHUB_WORKSPACE/$INPUT_CONFIG_PATH"
 fi
 
+echo "before: $INPUT_COMMIT_BEFORE"
+echo "config: $CONFIG"
+
 echo running gitleaks "$(gitleaks --version) with the following command👇"
 
 DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
 
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
-  echo "before: $INPUT_COMMIT_BEFORE"
+  
   echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact $CONFIG
   CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact $CONFIG)
 elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
