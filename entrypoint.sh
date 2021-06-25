bfe911edce6aec9291add590ed263e3b08c83ada
Zach Stone <zach@giantswarm.io>
2021-06-25 12:05:04 +0200

    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,31 +4,26 @@ INPUT_CONFIG_PATH="$1"
 INPUT_COMMIT_BEFORE="$2"
 CONFIG=""
 
-# check if a custom config have been provided
+# Check if a custom config has been provided.
 if [ -f "$GITHUB_WORKSPACE/$INPUT_CONFIG_PATH" ]; then
   CONFIG=" --config-path=$GITHUB_WORKSPACE/$INPUT_CONFIG_PATH"
 fi
 
-echo running gitleaks "$(gitleaks --version) with the following command👇"
-
 DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
 
+# Use the base branch for pull requests and the "before" commit for pushes as the scan boundary.
 case "$GITHUB_EVENT_NAME" in
  "push")         TARGET_REF="$INPUT_COMMIT_BEFORE" ;;
  "pull_request") TARGET_REF="remotes/origin/$GITHUB_BASE_REF" ;;
  *)              echo "Unsupported event type: $GITHUB_EVENT_NAME"; exit 1 ;;
 esac
 
-# if [ "$GITHUB_EVENT_NAME" = "push" ]
-# then
-#   TARGET_REF="$INPUT_COMMIT_BEFORE"
-# elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
-# then 
-#   TARGET_REF="remotes/origin/$GITHUB_BASE_REF"
-# fi
-
 echo "Scanning commits back to $TARGET_REF"
+
+# Create list of commits since the target ref.
 git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" $TARGET_REF... > commit_list.txt
+
+echo running gitleaks "$(gitleaks --version) with the following command👇"
 echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
 CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG)
 
